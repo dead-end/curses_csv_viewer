c0b8accbb6ef9f7a7d5b0a6f0c417f7a69341480
besen <besen@computer>
2018-07-05 00:00:16 +0200

    new function win_move

--- src/demo_windows.c
+++ src/demo_windows.c
@@ -14,32 +14,6 @@ WINDOW* win_footer;
 static char string[STR_SIZE];
 
 /**
- * The function refreshes all windows that are visible. If
- * the terminal is too small, some windows disappear.
- */
-static void refresh_all(const int win_y, const int win_x) {
-
-	if (win_x > 0) {
-
-		wnoutrefresh(stdscr);
-
-		if (win_y >= 1) {
-			wnoutrefresh(win_header);
-		}
-
-		if (win_y >= 2) {
-			wnoutrefresh(win_main);
-		}
-
-		if (win_y >= 3) {
-			wnoutrefresh(win_footer);
-		}
-
-		doupdate();
-	}
-}
-
-/**
  * The function prints some infos to all of the windows to see
  * if they are updated properly,
  */
@@ -53,14 +27,80 @@ static void print_str(const int x, const char* value, const int win_x) {
 }
 
 /**
+ * The function refreshes all windows that are visible. If
+ * the terminal is too small, some windows disappear.
+ */
+static void refresh_all(const int win_y, const int win_x) {
+
+	if (win_x > 0) {
+
+		if (wnoutrefresh(stdscr) != OK) {
+			fprintf(stderr, "Unable to refresh the stdscr!\n");
+			return;
+		}
+
+		if (win_y >= 1 && wnoutrefresh(win_header) != OK) {
+			fprintf(stderr, "Unable to refresh the header win!\n");
+			return;
+		}
+
+		if (win_y >= 2 && wnoutrefresh(win_main) != OK) {
+			fprintf(stderr, "Unable to refresh the main win!\n");
+			return;
+		}
+
+		if (win_y >= 3 && wnoutrefresh(win_footer) != OK) {
+			fprintf(stderr, "Unable to refresh the footer win!\n");
+			return;
+		}
+
+		if (doupdate() != OK) {
+			fprintf(stderr, "Unable to update all wins!\n");
+			return;
+		}
+	}
+}
+
+/**
+ * The function moves the window to the new position, if it
+ * has changed.
+ */
+void win_move(WINDOW *win, const int to_y, const int to_x) {
+	int from_y, from_x;
+
+	//
+	// Check whether the position changed.
+	//
+	getbegyx(win, from_y, from_x);
+	if (from_y == to_y && from_x == to_x) {
+		fprintf(stderr, "Win position has not changed!\n");
+		return;
+	}
+
+	fprintf(stderr, "from y: %d x: %d to y: %d x: %d\n", from_y, from_x, to_y, to_x);
+
+	//
+	// Do the actual moving.
+	//
+	if (mvwin(win, to_y, to_x) != OK) {
+		fprintf(stderr, "Unable to move window!\n");
+		exit(1);
+	}
+}
+
+/**
  * The function resizes all windows. The footer window has to be moved.
  */
-static void do_resize(const int win_y, const int win_x) {
+void do_resize_windows(const int win_y, const int win_x) {
 
 	if (win_x == 0 || win_y == 0) {
 		return;
 	}
 
+	//
+	// The terminal has a min height of 1, so the header is always shown
+	// and always at the same position.
+	//
 	if (wresize(win_header, 1, win_x) != OK) {
 		fprintf(stderr, "Unable to resize header window!\n");
 		exit(1);
@@ -71,7 +111,8 @@ static void do_resize(const int win_y, const int win_x) {
 	// window. If so, move it back.
 	//
 	if (win_y >= 2) {
-		int y = win_y == 2 ? 1 : win_y - 2;
+
+		int y = (win_y == 2 ? 1 : win_y - 2);
 
 		//
 		// If win_y == 2 then the footer disappeared.
@@ -81,29 +122,21 @@ static void do_resize(const int win_y, const int win_x) {
 			exit(1);
 		}
 
-		if (mvwin(win_main, 1, 0) != OK) {
-			fprintf(stderr, "Unable to move main window!\n");
-			exit(1);
-		}
+		win_move(win_main, 1, 0);
 	}
 
 	//
-	//
+	// The footer is only visible if the terminal has at least 3 lines.
 	//
 	if (win_y >= 3) {
+
 		if (wresize(win_footer, 1, win_x) != OK) {
 			fprintf(stderr, "Unable to resize footer window!\n");
 			exit(1);
 		}
 
-		if (mvwin(win_footer, win_y - 1, 0) != OK) {
-			fprintf(stderr, "Unable to move footer window!\n");
-			exit(1);
-		}
+		win_move(win_footer, win_y - 1, 0);
 	}
-
-	snprintf(string, STR_SIZE, "x: %03d y: %03d", win_x, win_y);
-	print_str(8, string, win_x);
 }
 
 /**
@@ -154,7 +187,9 @@ int main() {
 
 		} else if (key_input == KEY_RESIZE) {
 			getmaxyx(stdscr, win_y, win_x);
-			do_resize(win_y, win_x);
+			do_resize_windows(win_y, win_x);
+			snprintf(string, STR_SIZE, "x: %03d y: %03d", win_x, win_y);
+			print_str(8, string, win_x);
 			refresh_all(win_y, win_x);
 
 		} else {
