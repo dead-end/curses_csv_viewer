96bd48a697c807410d59427265a34547721cd26d
besen <besen@computer>
2018-07-04 22:36:23 +0200

    updated the demo program

--- src/demo_subwin.c
+++ src/demo_subwin.c
@@ -1,133 +1,109 @@
 /*
  * demo_subwin.c
  *
- * The demo program is used to test the behavior of windows
- * and subwindows. There are some problems:
- *
- * - Resizing (enlarging) of subwindows does not work properly.
- *   The background will not be updated.
- *
- * - Resizing the window to 1 line, results in windows with 0
- *   height. The should be avoided.
  */
 #include <ncursesw/ncurses.h>
 #include <stdlib.h>
+#include <string.h>
 
 WINDOW* win_header;
 WINDOW* win_main;
 WINDOW* win_footer;
 
-//
-// using subwin does not work
-//
-#define NEW_WIN
-
 #define STR_SIZE 20
 static char string[STR_SIZE];
 
 /**
- * The function refreshes all windows.
+ * The function refreshes all windows that are visible. If
+ * the terminal is too small, some windows disappear.
  */
-static void refresh_all() {
+static void refresh_all(const int win_y, const int win_x) {
 
-#ifdef NEW_WIN
-	int win_y, win_x;
+	if (win_x > 0) {
 
-	getmaxyx(stdscr, win_y, win_x);
+		wnoutrefresh(stdscr);
 
-	refresh();
+		if (win_y >= 1) {
+			wnoutrefresh(win_header);
+		}
 
-	if (win_y >= 1) {
-		wrefresh(win_header);
+		if (win_y >= 2) {
+			wnoutrefresh(win_main);
+		}
+
+		if (win_y >= 3) {
+			wnoutrefresh(win_footer);
+		}
+
+		doupdate();
 	}
-	if (win_y >= 2) {
-		wrefresh(win_main);
-	}
-	if (win_y >= 3 && win_x >= 0) {
-		wrefresh(win_footer);
-	}
-#else
-	touchwin(stdscr);
-
-	wrefresh(win_header);
-	wrefresh(win_main);
-	wrefresh(win_footer);
-
-	//
-	// On enlarging the terminal the background is not updated.
-	//
-	wbkgd(win_header, COLOR_PAIR(1));
-	wbkgd(win_main, COLOR_PAIR(2));
-	wbkgd(win_footer, COLOR_PAIR(1));
-#endif
 }
 
 /**
  * The function prints some infos to all of the windows to see
  * if they are updated properly,
  */
-static void print_str(const int x, const char* value) {
+static void print_str(const int x, const char* value, const int win_x) {
 
-	if (mvwaddstr(win_header, 0, x, value) != OK) {
-		fprintf(stderr, "Unable to print to header window!\n");
-		exit(1);
-	}
-
-	if (mvwaddstr(win_main, 0, x, value) != OK) {
-		fprintf(stderr, "Unable to print main to window!\n");
-		exit(1);
-	}
-
-	if (mvwaddstr(win_footer, 0, x, value) != OK) {
-		fprintf(stderr, "Unable to print to footer window!\n");
-		exit(1);
-	}
-}
-
-/**
- * ensure that the window does not disappear.
- */
-static void do_resize_win(WINDOW* win, const int win_y, const int win_x) {
-	if (win_y > 0 && win_x > 0) {
-		if (wresize(win, win_y, win_x) != OK) {
-			fprintf(stderr, "Unable to resize window!\n");
-			exit(1);
-		}
+	if (x + (int) strlen(value) <= win_x) {
+		mvwaddstr(win_header, 0, x, value);
+		mvwaddstr(win_main, 0, x, value);
+		mvwaddstr(win_footer, 0, x, value);
 	}
 }
 
 /**
  * The function resizes all windows. The footer window has to be moved.
  */
-static void do_resize() {
-	int win_y, win_x;
+static void do_resize(const int win_y, const int win_x) {
 
+	if (win_x == 0 || win_y == 0) {
+		return;
+	}
 
-	getmaxyx(stdscr, win_y, win_x);
-
-	do_resize_win(win_header, 1, win_x);
-	do_resize_win(win_main, win_y - 2, win_x);
-	do_resize_win(win_footer, 1, win_x);
+	if (wresize(win_header, 1, win_x) != OK) {
+		fprintf(stderr, "Unable to resize header window!\n");
+		exit(1);
+	}
 
 	//
 	// Ensure that the main window is not pushed outside the
 	// window. If so, move it back.
 	//
-	if (win_y >= 2 && mvwin(win_main, 1, 0) != OK) {
-		fprintf(stderr, "Unable to move window!\n");
-		exit(1);
+	if (win_y >= 2) {
+		int y = win_y == 2 ? 1 : win_y - 2;
+
+		//
+		// If win_y == 2 then the footer disappeared.
+		//
+		if (wresize(win_main, y, win_x) != OK) {
+			fprintf(stderr, "Unable to resize main window!\n");
+			exit(1);
+		}
+
+		if (mvwin(win_main, 1, 0) != OK) {
+			fprintf(stderr, "Unable to move main window!\n");
+			exit(1);
+		}
 	}
 
 	//
-	// mvwin does not work on enlarging with subwins.
 	//
-	if (mvwin(win_footer, win_y - 1, 0) != OK) {
-		fprintf(stderr, "Unable to move window!\n");
-		exit(1);
+	//
+	if (win_y >= 3) {
+		if (wresize(win_footer, 1, win_x) != OK) {
+			fprintf(stderr, "Unable to resize footer window!\n");
+			exit(1);
+		}
+
+		if (mvwin(win_footer, win_y - 1, 0) != OK) {
+			fprintf(stderr, "Unable to move footer window!\n");
+			exit(1);
+		}
 	}
 
 	snprintf(string, STR_SIZE, "x: %03d y: %03d", win_x, win_y);
-	print_str(8, string);
+	print_str(8, string, win_x);
 }
 
 /**
@@ -145,15 +121,9 @@ int main() {
 
 	getmaxyx(stdscr, win_y, win_x);
 
-#ifdef NEW_WIN
 	win_header = newwin(1, win_x, 0, 0);
 	win_main = newwin(win_y - 2, win_x, 1, 0);
 	win_footer = newwin(1, win_x, win_y - 1, 0);
-#else
-	win_header = subwin(stdscr, 1, win_x, 0, 0);
-	win_main = subwin(stdscr, win_y - 2, win_x, 1, 0);
-	win_footer = subwin(stdscr, 1, win_x, win_y - 1, 0);
-#endif
 
 	wbkgd(win_header, COLOR_PAIR(1));
 	wbkgd(win_main, COLOR_PAIR(2));
@@ -167,9 +137,9 @@ int main() {
 	mvwaddstr(win_footer, 0, 0, "footer");
 
 	snprintf(string, STR_SIZE, "x: %03d y: %03d", win_x, win_y);
-	print_str(8, string);
+	print_str(8, string, win_x);
 
-	refresh_all();
+	refresh_all(win_y, win_x);
 
 	while (true) {
 
@@ -183,13 +153,14 @@ int main() {
 			break;
 
 		} else if (key_input == KEY_RESIZE) {
-			do_resize();
-			refresh_all();
+			getmaxyx(stdscr, win_y, win_x);
+			do_resize(win_y, win_x);
+			refresh_all(win_y, win_x);
 
 		} else {
 			snprintf(string, STR_SIZE, "key: %d", key_input);
-			print_str(23, string);
-			refresh_all();
+			print_str(23, string, win_x);
+			refresh_all(win_y, win_x);
 		}
 	}
 
