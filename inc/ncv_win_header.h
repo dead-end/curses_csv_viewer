a759eeddea6c18248739fdb2fc74cc568d0c105a
besen <besen@computer>
2018-07-16 09:41:19 +0200

    rename header to win_header

---
+++ inc/ncv_win_header.h
@@ -0,0 +1,15 @@
+/*
+ * ncv_win_header.h
+ */
+
+#ifndef INC_NCV_WIN_HEADER_H_
+#define INC_NCV_WIN_HEADER_H_
+
+//
+// exported functions
+//
+void header_init();
+
+void header_resize();
+
+#endif /* INC_NCV_WIN_HEADER_H_ */
