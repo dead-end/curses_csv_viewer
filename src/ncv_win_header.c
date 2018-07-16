7e6a1a3a946f613d686e6df6cb1251f60ed31621
besen <besen@computer>
2018-07-16 10:24:35 +0200

    comments and renaming

--- src/ncv_win_header.c
+++ src/ncv_win_header.c
@@ -14,12 +14,21 @@
 #define HEADER_MIN_SIZE 10
 
 /***************************************************************************
+ * Currently the win_header_init() function only prints the label.
+ **************************************************************************/
+
+#define win_header_print_label() win_header_init()
+
+/***************************************************************************
  * The function is called to initialize the header window. If the window
  * size is OK, the label is printed.
  **************************************************************************/
 
 void win_header_init() {
 
+	//
+	// Filter window is on the right side.
+	//
 	if (getmaxx(stdscr) - HEADER_MIN_SIZE - WIN_FILTER_SIZE > 0) {
 		mvwaddstr(win_header, 0, 0, HEADER_LABEL);
 	}
@@ -37,15 +46,19 @@ void win_header_resize() {
 	//
 	if (getmaxx(stdscr) - WIN_FILTER_SIZE > 0) {
 
+		//
+		// Try to resize the window and stop if the size has not changed.
+		//
 		if (!ncurses_win_resize(win_header, 1, getmaxx(stdscr) - WIN_FILTER_SIZE)) {
 			print_debug_str("win_header_resize() Window size has not changed!\n");
 			return;
 		}
 
+		//
+		// Remove garbage and print the label
+		//
 		wclear(win_header);
 
-		if (getmaxx(stdscr) - HEADER_MIN_SIZE - WIN_FILTER_SIZE > 0) {
-			mvwaddstr(win_header, 0, 0, HEADER_LABEL);
-		}
+		win_header_print_label();
 	}
 }
