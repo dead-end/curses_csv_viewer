6bd37d48c3614614d0a450a470be01e5fc70c864
besen <besen@computer>
2018-07-17 10:01:08 +0200

    after refactoring

--- inc/ncv_win_footer.h
+++ inc/ncv_win_footer.h
@@ -9,8 +9,14 @@
 //
 // exported functions
 //
-void win_footer_content_print(const char* filename, const s_table *table, const s_field *cursor);
+void win_footer_init();
 
 void win_footer_resize();
+
+void win_footer_refresh_no();
+
+void win_footer_free();
+
+void win_footer_content_print(const char* filename, const s_table *table, const s_field *cursor);
 
 #endif /* INC_NCV_FOOTER_H_ */
