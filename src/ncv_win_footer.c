5b4631ff80647810ea43c844f08a9bd12bb38561
besen <besen@computer>
2018-07-17 10:09:01 +0200

    after refactoring

--- src/ncv_win_footer.c
+++ src/ncv_win_footer.c
@@ -1,6 +1,5 @@
 /*
  * ncv_win_footer.c
- *
  */
 
 #include "ncv_common.h"
@@ -11,6 +10,92 @@
 // Size of the string buffer.
 //
 #define FOOTER_WIN_MAX 1024
+
+//
+// The footer is only visible if the terminal has at least 3 rows. One row
+// for the header and one row for the table and one row for the footer.
+//
+#define WIN_FOOTER_MIN_SIZE (getmaxy(stdscr) >= 3)
+
+//
+// Definition of the footer window.
+//
+static WINDOW* win_footer;
+
+/***************************************************************************
+ * The function is called to initialize the footer window.
+ **************************************************************************/
+
+void win_footer_init() {
+
+	//
+	// Create the footer window in the last row.
+	//
+	win_footer = ncurses_win_create(1, getmaxx(stdscr), getmaxy(stdscr) - 1, 0);
+
+	//
+	// Set the footer window background.
+	//
+	ncurses_attr_back(win_footer, COLOR_PAIR(CP_STATUS), A_REVERSE);
+}
+
+/***************************************************************************
+ * The function is called on resizing the terminal window.
+ **************************************************************************/
+
+void win_footer_resize() {
+
+	//
+	// Ensure the minimum size of the window.
+	//
+	if (WIN_FOOTER_MIN_SIZE) {
+		print_debug_str("win_footer_resize() Do resize the window!\n");
+
+		//
+		// Resize the window.
+		//
+		ncurses_win_resize(win_footer, 1, getmaxx(stdscr));
+
+		//
+		// Move the footer to the bottom of the terminal. This is necessary
+		// even if the size of the window does not change.
+		//
+		ncurses_win_move(win_footer, getmaxy(stdscr) - 1, 0);
+	}
+}
+
+/***************************************************************************
+ * The function does a refresh with no update if the terminal is large
+ * enough.
+ **************************************************************************/
+
+void win_footer_refresh_no() {
+
+	//
+	// Ensure the minimum size of the window.
+	//
+	if (WIN_FOOTER_MIN_SIZE) {
+		print_debug_str("win_footer_refresh_no() Do refresh the window!\n");
+
+		//
+		// Do the refresh.
+		//
+		if (wnoutrefresh(win_footer) != OK) {
+			print_exit_str("win_footer_refresh_no() Unable to refresh the window!\n");
+		}
+	}
+}
+
+/***************************************************************************
+ * The function frees the allocated resources.
+ **************************************************************************/
+
+void win_footer_free() {
+
+	if (delwin(win_footer) != OK) {
+		print_exit_str("win_footer_free() Unable to delete footer window!\n");
+	}
+}
 
 /***************************************************************************
  * The function prints the footer line, which consists of the current row /
@@ -73,32 +158,5 @@ void win_footer_content_print(const char *filename, const s_table *table, const 
 
 	if (strlen_row_col + strlen_filename <= max_width) {
 		mvwaddstr(win_footer, 0, 0, buf);
-	}
-}
-
-/***************************************************************************
- * The function is called on resizing the terminal window.
- **************************************************************************/
-
-void win_footer_resize() {
-	print_debug_str("win_footer_resize() Start resize.");
-
-	//
-	// The footer is only visible if the terminal has at least 3 rows. One
-	// row for the header and one row for the table and one row for the
-	// footer.
-	//
-	if (getmaxy(stdscr) >= 3) {
-
-		//
-		// Resize the window.
-		//
-		ncurses_win_resize(win_footer, 1, getmaxx(stdscr));
-
-		//
-		// Move the footer to the bottom of the terminal. This is necessary
-		// even if the size of the window does not change.
-		//
-		ncurses_win_move(win_footer, getmaxy(stdscr) - 1, 0);
 	}
 }
