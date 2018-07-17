1b1e5702aff3f6201f461739fd4424cadc17e924
besen <besen@computer>
2018-07-17 10:09:01 +0200

    after refactoring

--- inc/ncv_win_table.h
+++ inc/ncv_win_table.h
@@ -13,12 +13,16 @@ void win_table_init();
 
 void win_table_resize();
 
+void win_table_refresh_no();
+
+void win_table_free();
+
 void win_table_content_init(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_field *cursor);
 
 void win_table_content_resize(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_field *cursor);
 
 bool win_table_content_mv_cursor(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_field *cursor, const int key_input);
 
-void win_table_content_print(WINDOW *win, const s_table *table, const s_table_part *row_table_part, const s_table_part *col_table_part, const s_field *cursor);
+void win_table_content_print(const s_table *table, const s_table_part *row_table_part, const s_table_part *col_table_part, const s_field *cursor);
 
 #endif /* INC_NCV_WIN_TABLE_H_ */
