cf6b1169e37656c7a802a9099e4cb191fe4b0554
besen <besen@computer>
2018-07-19 08:55:02 +0200

    added s_cursor struct

--- inc/ncv_win_table.h
+++ inc/ncv_win_table.h
@@ -9,20 +9,35 @@
 #include "ncv_table.h"
 #include "ncv_table_part.h"
 
+//
+// Structure for the cursor.
+//
+typedef struct s_cursor {
+
+	int row;
+
+	int col;
+
+	bool visible;
+
+} s_cursor;
+
 void win_table_init();
 
 void win_table_resize();
+
+void win_table_refresh();
 
 void win_table_refresh_no();
 
 void win_table_free();
 
-void win_table_content_init(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_field *cursor);
+void win_table_content_init(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_cursor *cursor);
 
-void win_table_content_resize(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_field *cursor);
+void win_table_content_resize(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_cursor *cursor);
 
-bool win_table_content_mv_cursor(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_field *cursor, const int key_input);
+bool win_table_content_mv_cursor(const s_table *table, s_table_part *row_table_part, s_table_part *col_table_part, s_cursor *cursor, const int key_input);
 
-void win_table_content_print(const s_table *table, const s_table_part *row_table_part, const s_table_part *col_table_part, const s_field *cursor);
+void win_table_content_print(const s_table *table, const s_table_part *row_table_part, const s_table_part *col_table_part, const s_cursor *cursor);
 
 #endif /* INC_NCV_WIN_TABLE_H_ */
