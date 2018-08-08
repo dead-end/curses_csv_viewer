8f690d6732d52cf75029bd8dffb127fbb3489ca1
dead end <dead.end@inbox.ru>
2018-08-08 22:27:29 +0200

    added comments and license

--- inc/ncv_ncurses.h
+++ inc/ncv_ncurses.h
@@ -1,27 +1,48 @@
 /*
- * ncv_ncurses.h
+ * MIT License
+ *
+ * Copyright (c) 2018 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef INC_NCV_NCURSES_H_
 #define INC_NCV_NCURSES_H_
 
-#include "ncv_common.h"
+/***************************************************************************
+ * Check for control modifier. Usage: CTRL('f')
+ **************************************************************************/
 
-//
-// Check for control modifier. Usage: CTRL('f')
-//
 #ifndef CTRL
 #define CTRL(x) ((x) & 0x1f)
 #endif
 
-//
-// CTRL chars are defined between 0 and 31 (0x1f)
-//
+/***************************************************************************
+ * CTRL chars are defined between 0 and 31 (0x1f)
+ **************************************************************************/
+
 #define is_ctrl_char(c) ((c) < 32)
 
-//
-// Color pairs
-//
+/***************************************************************************
+ * The indices of color pairs
+ **************************************************************************/
+
 #define CP_TABLE 1
 
 #define CP_TABLE_HEADER 2
@@ -58,9 +79,6 @@ void ncurses_attr_on(WINDOW *win, s_attr_reset *attr_reset, const int attr);
 
 void ncurses_attr_off(WINDOW *win, s_attr_reset *attr_reset);
 
-//
-// Ncurses initializazion functions.
-//
 void ncurses_init(const bool monochrom, const bool use_initscr);
 
 void ncurses_free();
@@ -69,13 +87,10 @@ chtype ncurses_attr_color(const chtype color, const chtype alt);
 
 void ncurses_attr_back(WINDOW *win, const chtype color, const chtype alt);
 
-//
-// Window operations
-//
 WINDOW *ncurses_win_create(const int rows, const int cols, const int begin_y, const int begin_x);
 
 void ncurses_win_move(WINDOW *win, const int to_y, const int to_x);
 
 bool ncurses_win_resize(WINDOW *win, const int to_y, const int to_x);
 
-#endif /* INC_NCV_NCURSES_H_ */
+#endif
--- inc/ncv_table_part.h
+++ inc/ncv_table_part.h
@@ -1,12 +1,32 @@
 /*
- * ncv_table_part.h
+ * MIT License
+ *
+ * Copyright (c) 2018 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef INC_NCV_TABLE_PART_H_
 #define INC_NCV_TABLE_PART_H_
 
 //
-// Direction definitions that allow to be used in increments of loops
+// Defintion of directions (can be used to increment in loops)
 //
 #define DIR_FORWARD 1
 
@@ -14,8 +34,8 @@
 
 /***************************************************************************
  * The table has two s_table_part structures, one for the row and one for
- * the column. The structure defines which part of the table is visible and
- * which fields are truncated.
+ * the column. The structure defines which fields of the table are visible
+ * and which fields are truncated.
  ***************************************************************************/
 
 typedef struct s_table_part {
@@ -51,10 +71,6 @@ typedef struct s_table_part {
 
 } s_table_part;
 
-/***************************************************************************
- * Macro definitions
- **************************************************************************/
-
 //
 // The macro is called with a table part and an index and it checks whether
 // the given index is before the table part first value.
@@ -85,12 +101,8 @@ typedef struct s_table_part {
 //
 #define is_not_truncated_and_last(p, i) ((p)->truncated == -1 && (i) == (p)->last)
 
-/***************************************************************************
- * Function definitions
- **************************************************************************/
-
 void s_table_part_update(s_table_part *table_part, const int *sizes, const int index_start, const int index_max, const int direction, const int win_size);
 
 bool adjust_dir_on_resize(s_table_part *table_part, const int table_end);
 
-#endif /* INC_NCV_TABLE_PART_H_ */
+#endif
