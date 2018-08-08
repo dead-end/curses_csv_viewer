8383634ef8b3bb75763ec8166e1b0a843be10cb7
dead end <dead.end@inbox.ru>
2018-08-08 21:24:10 +0200

    added comments and the license

--- inc/ncv_corners.h
+++ inc/ncv_corners.h
@@ -1,20 +1,32 @@
 /*
- * ncv_corners.h
+ * MIT License
+ *
+ * Copyright (c) 2018 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef INC_NCV_CORNERS_H_
 #define INC_NCV_CORNERS_H_
 
-#include "ncv_common.h"
-#include "ncv_table.h"
-#include "ncv_table_part.h"
-
-/***************************************************************************
- * Function definitions
- **************************************************************************/
-
 void s_corner_inits(const int no_rows, const int no_columns);
 
 void s_corner_print(WINDOW *win, const s_field *idx, const s_field *win_field, const s_field *win_field_end, const s_table_part *row_table_part, const s_table_part *col_table_part);
 
-#endif /* INC_NCV_CORNERS_H_ */
+#endif
