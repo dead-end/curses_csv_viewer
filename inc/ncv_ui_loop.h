13bf6d8837bf989319843c6245254eea5eb818b1
dead end <dead.end@inbox.ru>
2018-08-08 22:07:01 +0200

    added license

--- inc/ncv_ui_loop.h
+++ inc/ncv_ui_loop.h
@@ -1,5 +1,25 @@
 /*
- * file: ncv_curses.h
+ * MIT License
+ *
+ * Copyright (c) 2018 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef INC_NCV_UI_LOOP_H_
@@ -7,10 +27,6 @@
 
 #include "ncv_table.h"
 
-/***************************************************************************
- * Function definitions
- **************************************************************************/
-
 void ui_loop(s_table *table, const char *filename);
 
-#endif /* INC_NCV_UI_LOOP_H_ */
+#endif
