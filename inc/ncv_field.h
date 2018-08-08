284a68df64cf1ba974d3e6158c4f549d20c4faa5
dead end <dead.end@inbox.ru>
2018-08-08 21:57:36 +0200

    added comments and the license

--- inc/ncv_field.h
+++ inc/ncv_field.h
@@ -1,18 +1,35 @@
 /*
- * ncv_field.h
+ * MIT License
+ *
+ * Copyright (c) 2018 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef INC_NCV_FIELD_H_
 #define INC_NCV_FIELD_H_
 
-#include "ncv_common.h"
-#include "ncv_table_part.h"
-
 /***************************************************************************
  * A field has two s_field_part structures, one for the row and one for the
  * column. The structure defines which part of the field is visible.
  * It has a start member, which is 0 if the field is not truncated and a
- * size, which is the (maybe truncated) row height or the (maybe truncated)
+ * size, which is less or equal than the row height or less or equal than the
  * column width.
  ***************************************************************************/
 
@@ -32,9 +49,7 @@ typedef struct s_field_part {
 
 } s_field_part;
 
-/***************************************************************************
- * Function definitions
- **************************************************************************/
+void print_field(WINDOW *win, wchar_t *ptr, s_field_part *row_field_part, s_field_part *col_field_part, const s_field *win_row_col);
 
 //
 // The functions are only visible for unit tests.
@@ -43,9 +58,4 @@ void s_field_part_update(const s_table_part *table_part, const int size, const i
 
 wchar_t *get_field_line(wchar_t *str_ptr, wchar_t *buffer, const s_field_part *col_field_part);
 
-//
-// The exported functions.
-//
-void print_field(WINDOW *win, wchar_t *ptr, s_field_part *row_field_part, s_field_part *col_field_part, const s_field *win_row_col);
-
-#endif /* INC_NCV_FIELD_H_ */
+#endif
