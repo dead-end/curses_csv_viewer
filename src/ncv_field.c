c14d22142b687f83951a0636365164aed692e46b
dead end <dead.end@inbox.ru>
2018-08-10 19:24:20 +0200

    added comments and license

--- src/ncv_field.c
+++ src/ncv_field.c
@@ -1,5 +1,25 @@
 /*
- * ncv_field.c
+ * MIT License
+ *
+ * Copyright (c) 2018 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include "ncv_field.h"
@@ -27,12 +47,14 @@ void s_field_part_update(const s_table_part *table_part, const int index, const 
 
 		//
 		// If the row is truncated the first or the last lines are skipped.
-		// if the column is truncated the first or the last chars of each
+		// If the column is truncated the first or the last chars of each
 		// line of the field is skipped.
 		//
 		// If the table part last is truncated, the last lines / chars are
 		// skipped. If the table part first is truncated, the first lines
 		// chars are skipped.
+		//
+		// Special case one truncated field => first == last
 		//
 		if (table_part->first != table_part->last && table_part->truncated == table_part->first) {
 			field_part->start = size - table_part->size;
