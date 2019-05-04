6746fb1d7a309febb65e505b2618597242213f8f
dead end <dead.end@inbox.ru>
2019-05-04 09:49:21 +0200

    derby db

--- derby-db/create.sql
+++ derby-db/create.sql
@@ -1,6 +1,5 @@
 create table mytab(idx int, name varchar(40));
-insert into mytab values (1,'Derby DB'); 
-insert into mytab values (2,'PostgreSQL'); 
-insert into mytab values (3,'Maria DB'); 
-insert into mytab values (4,'Mysql'); 
-select * from mytab;
+insert into mytab values (1,'Derby DB');
+insert into mytab values (2,'PostgreSQL');
+insert into mytab values (3,'Maria DB');
+insert into mytab values (4,'Mysql');
