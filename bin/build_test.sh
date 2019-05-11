ee02d21327b55d4c985842e0032ce0174f840e9e
dead end <dead.end@inbox.ru>
2019-05-11 10:50:44 +0200

    moved script

--- bin/build_test.sh
+++ bin/build_test.sh
@@ -2,29 +2,43 @@
 
 set -ue
 
+bin_dir="bin"
+
 ###############################################################################
 # error function
 ###############################################################################
 
 do_exit () { 
-   echo "ERROR: $1 failed" 
+   echo "ERROR: $1" 
    exit 1
 }
+
+###############################################################################
+# check the directory
+###############################################################################
+
+if [ ! -f makefile ] ; then
+  do_exit "File not found in the current directory: makefile"
+fi
+
+if [ ! -f "${bin_dir}/cmake_build.sh" ] ; then
+  do_exit "The build script does not exist: ${bin_dir}/cmake_build.sh"
+fi
 
 ###############################################################################
 # do builds
 ###############################################################################
 
-make clean                || do_exit "make clean"
+make clean                || do_exit "make clean failed"
 
-make DEBUG="-DDEBUG -g"   || do_exit "make debug"
+make DEBUG="-DDEBUG -g"   || do_exit "make debug failed"
 
-make clean                || do_exit "make clean"
+make clean                || do_exit "make clean failed"
 
-make                      || do_exit "make"
+make                      || do_exit "make failed"
 
-sh cmake_build.sh debug   || do_exit "cmake debug"
+sh "${bin_dir}/cmake_build.sh" debug   || do_exit "cmake debug failed"
 
-sh cmake_build.sh release || do_exit "cmake release"
+sh "${bin_dir}/cmake_build.sh" release || do_exit "cmake release failed"
 
 exit 0
