7c7e1d2b99197d63c0f65d8deef4f179b35bb7eb
dead end <dead.end@inbox.ru>
2019-05-11 11:39:10 +0200

    added version infos

--- bin/pkg-deps.sh
+++ bin/pkg-deps.sh
@@ -6,6 +6,32 @@
 
 set -ue
 
+DEBUG=
+
+###############################################################################
+# logging function
+###############################################################################
+
+log () {
+  level="${1}"
+  msg="${2}"
+  
+  if [ "${level}" = "debug" ] ; then
+    if [ -n "${DEBUG}" ] ; then
+      echo "DEBUG: ${msg}"
+    fi
+    
+  elif [ "${level}" = "info" ] ; then
+    echo "INFO: ${msg}"
+
+  elif [ "${level}" = "error" ] ; then
+    echo "ERROR: ${msg}"
+    exit 1
+  else
+    log "error" "EXIT: Unknown level: ${level}"
+  fi
+}
+
 ###############################################################################
 # Ensure that the script is called with an executable 
 ###############################################################################
@@ -18,8 +44,7 @@ fi
 executable="${1}"
 
 if [ ! -x "${executable}" ] ; then
-  echo "The file: ${executable} is not an executable!"
-  exit 1
+  log "error" "The file: ${executable} is not an executable!"
 fi
 
 echo "Processing: ${executable}"
@@ -33,17 +58,40 @@ raw_dep_libs=$(objdump -p "${executable}")
 
 dep_libs=$(echo "${raw_dep_libs}" | grep NEEDED | sed 's# *NEEDED *##')
 
-deps=
+log "debug" "dep_libs: ${dep_libs}"
+
+dep_pkgs=
 
 for dep_lib in ${dep_libs} ; do
-  raw_dep=$(dpkg -S ${dep_lib})
-  dep=$(echo "${raw_dep}" | sed 's#:.*##')
-  deps="${deps}\n${dep}"
+
+  log "debug" "Processing: ${dep_lib}"
+  
+  pkgs_libs=$(dpkg -S ${dep_lib})
+  log "debug" "Packages and libs:\n${pkgs_libs}"
+  
+  pkgs=$(echo "${pkgs_libs}" | sed 's#:.*##')
+  log "debug" "Packages:\n${pkgs}"
+  
+  dep_pkgs="${dep_pkgs}\n${pkgs}"
 done
 
-deps=$(echo "$deps" | sort -u)
+###############################################################################
+# Create a unique list of dependend packages and get the version for each 
+# package. The extention in the version is removed.
+# (before: 6.1-1ubuntu1.18.04 after: 6.1)
+###############################################################################
 
-echo "Found dependencies: ${deps}"
+echo "Found dependencies:"
+
+dep_pkgs=$(echo "$dep_pkgs" | sort -u)
+
+for dep_pkg in ${dep_pkgs} ; do
+  version=$(dpkg -s "${dep_pkg}" | grep "Version:")
+  log "debug" "version: ${version}"
+  
+  version=$(echo "${version}" | sed 's#Version: ## ; s#-.*##')
+  echo "${dep_pkg} (${version})"
+done
 
 exit 0
 
