75834f28aca33753a7e63bc123dcd68f5b38293f
besen <besen@computer>
2019-07-20 21:48:14 +0200

    update unit tests

--- src/ut_table_part.c
+++ src/ut_table_part.c
@@ -51,15 +51,15 @@ static void check_table_part(const s_table_part *table_part, const int first, co
 static void test_table_part_update() {
 	s_table_part table_part;
 
-	print_debug_str("test_table_part_update() Start\n");
+	log_debug_str("Start");
 
 	//
 	// 1234567890 <- win size == 10
 	// |****|*|***|
 	//          ^
 	//
-	print_debug_str("test_table_part_update() test 1\n");
-	int sizes_1[3] = { 4, 1, 3 };
+	log_debug_str("Test 1");
+	const int sizes_1[3] = { 4, 1, 3 };
 	s_table_part_update(&table_part, sizes_1, 0, 3, E_DIR_FORWARD, 10);
 	check_table_part(&table_part, 0, 2, 2, 2);
 
@@ -68,8 +68,8 @@ static void test_table_part_update() {
 	// |****|***|*|
 	//          ^
 	//
-	print_debug_str("test_table_part_update() test 2\n");
-	int sizes_2[3] = { 4, 3, 1 };
+	log_debug_str("Test 2");
+	const int sizes_2[3] = { 4, 3, 1 };
 	s_table_part_update(&table_part, sizes_2, 0, 3, E_DIR_FORWARD, 10);
 	check_table_part(&table_part, 0, 1, -1, 0);
 
@@ -78,8 +78,8 @@ static void test_table_part_update() {
 	// |****|**|***|
 	//          ^
 	//
-	print_debug_str("test_table_part_update() test 3\n");
-	int sizes_3[3] = { 4, 2, 3 };
+	log_debug_str("Test 3");
+	const int sizes_3[3] = { 4, 2, 3 };
 	s_table_part_update(&table_part, sizes_3, 0, 3, E_DIR_FORWARD, 10);
 	check_table_part(&table_part, 0, 2, 2, 1);
 
@@ -88,8 +88,8 @@ static void test_table_part_update() {
 	// |****|*|***|
 	//            ^
 	//
-	print_debug_str("test_table_part_update() test 4\n");
-	int sizes_4[3] = { 4, 1, 3 };
+	log_debug_str("Test 4");
+	const int sizes_4[3] = { 4, 1, 3 };
 	s_table_part_update(&table_part, sizes_4, 0, 3, E_DIR_FORWARD, 20);
 	check_table_part(&table_part, 0, 2, -1, 0);
 
@@ -98,8 +98,8 @@ static void test_table_part_update() {
 	// |***|*|****|
 	//   ^
 	//
-	print_debug_str("test_table_part_update() test 5\n");
-	int sizes_5[3] = { 3, 1, 4 };
+	log_debug_str("Test 5");
+	const int sizes_5[3] = { 3, 1, 4 };
 	s_table_part_update(&table_part, sizes_5, 2, 3, E_DIR_BACKWARD, 10);
 	check_table_part(&table_part, 0, 2, 0, 2);
 
@@ -108,8 +108,8 @@ static void test_table_part_update() {
 	// |*|***|****|
 	//   ^
 	//
-	print_debug_str("test_table_part_update() test 6\n");
-	int sizes_6[3] = { 1, 3, 4 };
+	log_debug_str("Test 6");
+	const int sizes_6[3] = { 1, 3, 4 };
 	s_table_part_update(&table_part, sizes_6, 2, 3, E_DIR_BACKWARD, 10);
 	check_table_part(&table_part, 1, 2, -1, 0);
 
@@ -118,8 +118,8 @@ static void test_table_part_update() {
 	// |***|**|****|
 	//    ^
 	//
-	print_debug_str("test_table_part_update() test 7\n");
-	int sizes_7[3] = { 3, 2, 4 };
+	log_debug_str("Test 7");
+	const int sizes_7[3] = { 3, 2, 4 };
 	s_table_part_update(&table_part, sizes_7, 2, 3, E_DIR_BACKWARD, 10);
 	check_table_part(&table_part, 0, 2, 0, 1);
 
@@ -128,11 +128,11 @@ static void test_table_part_update() {
 	//         |***|*|****|
 	//         ^
 	//
-	print_debug_str("test_table_part_update() test 8\n");
-	int sizes_8[3] = { 3, 1, 4 };
+	log_debug_str("Test 8");
+	const int sizes_8[3] = { 3, 1, 4 };
 	s_table_part_update(&table_part, sizes_8, 2, 3, E_DIR_BACKWARD, 20);
 
-	print_debug_str("test_table_part_update() End\n");
+	log_debug_str("End");
 }
 
 /******************************************************************************
@@ -141,14 +141,13 @@ static void test_table_part_update() {
 
 int main() {
 
-	print_debug_str("ut_table_part.c - Start tests\n");
+	log_debug_str("Start");
 
-	// TODO: C
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "C");
 
 	test_table_part_update();
 
-	print_debug_str("ut_table_part.c - End tests\n");
+	log_debug_str("End");
 
 	return EXIT_SUCCESS;
 }
