b2129f8a5c983725caa66047b1bad8e96bf68ebc
dead end <dead.end@inbox.ru>
2019-10-12 22:09:33 +0200

    updated extended description

--- bin/create-deb.sh
+++ bin/create-deb.sh
@@ -77,9 +77,9 @@ Homepage: https://github.com/dead-end/curses_csv_viewer
 Depends: ${dependencies=}
 Maintainer: dead-end
 Description: Curses based csv file viewer
- Ccsvv is a viewer for csv (comma-separated values) data. It accepts input from a
- file or from stdin. It supports different field separators, multi line fields,
- escaping with " and filtering / sorting.
+ Ccsvv is a viewer for csv (comma-separated values) data. It accepts input from 
+ a file or from stdin. It supports different field separators, multi line 
+ fields, escaping with " and filtering / sorting.
 EOF
 
 #
